5b322c25cfc35ecf0c3c5368451f6454ddefef81
David <davidtorres-56@hotmail.com>
2024-12-04 18:48:22 +0100

    canonical 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import { Metadata } from "next";
 import { BannerComponent, FormModalComponent, ModalidadesComponent, PartnersComponent, ProgramsComponent, SpeackWithUs, TestimoniesComponent, WeContactUComponent } from "./components";
 import { FooterComponent } from "./components/footer/footer";
-import Head from 'next/head';
 
 const canonical = "https://cursosepegratis.es"
 export const metadata: Metadata = {
   title: 'Cursos 100% subvencionados',
   description: 'Descubre tu camino y expande tus conocimientos con nuestra amplia oferta de cursos de formación profesional y 100% subvencionados por instituciones estatales reconocidas como el SEPE, SOC, FUNDAE, Cámara de comercio ... No te piertas esta gran oportunidad',
+  alternates:{
+    canonical: canonical,
+  },
   
 }
 
@@ -15,24 +17,20 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <>
-    <Head>
-    <link rel="canonical" href={canonical} />
-    </Head>
-    <div className="">
-      <main className="flex flex-col">
-        {/* <BanerTopComponent /> */}
-        <BannerComponent />
-        <PartnersComponent />
-        <ModalidadesComponent />
-        <ProgramsComponent />
-        <TestimoniesComponent />
-        <SpeackWithUs />
-        <WeContactUComponent />
-        <FooterComponent />
-        <FormModalComponent />
-      </main>
-    </div>
-    </>
+
+      <div className="">
+        <main className="flex flex-col">
+          {/* <BanerTopComponent /> */}
+          <BannerComponent />
+          <PartnersComponent />
+          <ModalidadesComponent />
+          <ProgramsComponent />
+          <TestimoniesComponent />
+          <SpeackWithUs />
+          <WeContactUComponent />
+          <FooterComponent />
+          <FormModalComponent />
+        </main>
+      </div>
   );
 }

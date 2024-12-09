23954035350369d6eb98cd694411efa7fe36e0e3
David <davidtorres-56@hotmail.com>
2024-12-09 18:25:21 +0100

    google analitycs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { GoogleTagManager } from '@next/third-parties/google'
+import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google'
 
 
 const geistSans = localFont({
@@ -30,6 +30,7 @@ export default function RootLayout({
   return (
     <html lang="es">
       <GoogleTagManager gtmId="GTM-PRWWV43T"/>
+      <GoogleAnalytics gaId="G-5M11C6K7G4"/>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >

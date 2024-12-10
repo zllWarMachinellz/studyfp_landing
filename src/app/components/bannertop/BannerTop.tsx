f67daeaafc1fb84d1d0ce60303fa257f6a08858f
David <davidtorres-56@hotmail.com>
2024-12-10 13:18:16 +0100

    Banner top

--- src/app/components/bannertop/BannerTop.tsx
+++ src/app/components/bannertop/BannerTop.tsx
@@ -4,7 +4,7 @@ export const BanerTopComponent = () => {
   return (
     <div className="dark:bg-slate-100 bg-black dark:text-blue-950 text-white flex justify-center text-x py-1 bg-gradient-to-b from-slate-600 to-gray-900 dark:from-slate-50 dark:to-gray-300">
       <div className="">
-        <h2>La mejor desición para potenciar 🚀 tu futuro profesional</h2>
+        <h2>La mejor decisión para potenciar 🚀 tu futuro profesional</h2>
       </div>
     </div>
   )

ed7cfd375d3cc9ef17bd3d57529e0b4ca5867627
victorstudy <victorstudyformacion@gmail.com>
2025-03-04 18:36:43 +0100

    10 courses added on courses datebase

--- src/app/data/cursos.ts
+++ src/app/data/cursos.ts
@@ -32,68 +32,152 @@ const consorciSectorial = ["Empleados", "Virtual"]
 export const courses: course[] = [
     {
         id: '1',
-        img: '/imgs/cursos/depilacion_mecanica/depilacion_mecanica.webp',
-        codigo: "MF0345",
-        name: 'Depilación mecánica y decoloración del vello',
-        hours: 90,
-        entidad_subvencion: "/imgs/logos/consorci.png",
-        init_date: '20-11-2024',
-        end_date: '11-12-2024',
-        requirements: consorciEmpleados,
-        estado: false
+        img: '/imgs/cursos/ada/ada-contenido-digital-basico.webp',
+        codigo: "[FCOI15_1]",
+        name: 'Creación de contenidos digitales. Nivel básico',
+        hours: 40,
+        entidad_subvencion: "/imgs/logos/logo-socs.svg",
+        init_date: '17-03-2025',
+        end_date: '28-03-2025',
+        requirements: socAda,
+        estado: true
     },
     {
         id: '2',
-        img: '/imgs/cursos/ada/ada-informatica.webp',
-        codigo: "FCOI13",
-        name: 'Iniciación a la informática y en competencias digitales básicas',
+        img: '/imgs/cursos/ada/ada-gestion-contenido-digital.webp',
+        codigo: "[FCOI10_1]",
+        name: 'Búsqueda y gestión de la información digital. Nivel básico',
         hours: 40,
         entidad_subvencion: "/imgs/logos/logo-socs.svg",
-        init_date: '09-12-2024',
-        end_date: '20-12-2024',
+        init_date: 'Por definir',
+        end_date: 'Por definir',
         requirements: socAda,
         estado: true
     },
     {
         id: '3',
-        img: '/imgs/cursos/ada/ada-excel.webp',
-        codigo: "FCOI27",
-        name: 'Aplicación de la hoja de cálculo Excel. Nivel inicial',
+        img: '/imgs/cursos/ada/ada-comunicación-colaboracion-herramientas-digitales.webp',
+        codigo: "[FCOI10_1]",
+        name: 'Comunicación y colaboración con herramientas digitales. Nivel básico',
         hours: 40,
         entidad_subvencion: "/imgs/logos/logo-socs.svg",
-        init_date: '23-12-2024',
-        end_date: '16-01-2025',
+        init_date: 'Por definir',
+        end_date: 'Por definir',
         requirements: socAda,
         estado: true
     },
     {
         id: '4',
-        img: '/imgs/cursos/sectorial/sectorial-cliente-ingles.webp',
-        codigo: "COMT0013",
-        name: 'Atención al cliente del pequeño comercio en lengua extranjera, Inglés',
-        hours: 30,
-        entidad_subvencion: "/imgs/logos/consorci.png",
-        init_date: "Por definir",
-        end_date: "Por definir",
-        requirements: consorciSectorial,
+        img: '/imgs/cursos/ada/ada-herramientas-gestio-web.webp',
+        codigo: "[IFCT053PO_1]",
+        name: 'Herramientas de gestión web (gestión de contenidos)',
+        hours: 40,
+        entidad_subvencion: "/imgs/logos/logo-socs.svg",
+        init_date: 'Por definir',
+        end_date: 'Por definir',
+        requirements: socAda,
         estado: true
     },
     {
         id: '5',
-        img: '/imgs/cursos/sectorial/sectorial-neuromarketing.webp',
-        codigo: "COMM0014",
-        name: 'Neuromarketing curso virtual para empleados',
-        hours: 35,
-        entidad_subvencion: "/imgs/logos/consorci.png",
-        init_date: "Por definir",
-        end_date: "Por definir",
-        requirements: consorciSectorial,
+        img: '/imgs/cursos/ada/ada-informatica.webp',
+        codigo: "[FCOI13_2]",
+        name: 'Iniciación a la informática y en competencias digitales básicas',
+        hours: 40,
+        entidad_subvencion: "/imgs/logos/logo-socs.svg",
+        init_date: '14-04-2025',
+        end_date: '02-05-2025',
+        requirements: socAda,
         estado: true
     },
     {
         id: '6',
+        img: '/imgs/cursos/ada/ada-excel.webp',
+        codigo: "[FCOI28_2]",
+        name: 'Aplicación de la hoja de cálculo (Excel). Nivel inicial',
+        hours: 40,
+        entidad_subvencion: "/imgs/logos/logo-socs.svg",
+        init_date: '05-05-2025',
+        end_date: '16-05-2025',
+        requirements: socAda,
+        estado: true
+    },
+    {
+        id: '7',
+        img: '/imgs/cursos/ada/ada-excel-avanzado.webp',
+        codigo: "[FCOI27_2]",
+        name: 'Aplicación de la hoja de cálculo (Excel). Nivel avanzado',
+        hours: 40,
+        entidad_subvencion: "/imgs/logos/logo-socs.svg",
+        init_date: '19-05-2025',
+        end_date: '21-06-2025',
+        requirements: socAda,
+        estado: true
+    },
+    {
+        id: '8',
+        img: '/imgs/cursos/ada/ada-word-avanzado.webp',
+        codigo: "[ADGG084PO_2]",
+        name: 'Aplicación de documentos de texto (Word). Nivel avanzado',
+        hours: 40,
+        entidad_subvencion: "/imgs/logos/logo-socs.svg",
+        init_date: '31-03-2025',
+        end_date: '11-04-2025',
+        requirements: socAda,
+        estado: true
+    },
+    {
+        id: '9',
+        img: '/imgs/cursos/ada/ada-sap-modulsd.webp',
+        codigo: "[IFCT83_3]",
+        name: 'SAP - Módulo SD, ventas y distribución - nivel usuario',
+        hours: 40,
+        entidad_subvencion: "/imgs/logos/logo-socs.svg",
+        init_date: '07-03-2025',
+        end_date: '18-03-2025',
+        requirements: socAda,
+        estado: true
+    },
+    {
+        id: '10',
+        img: '/imgs/cursos/ada/ada-sap-modulomm.webp',
+        codigo: "[IFCT82_3]",
+        name: 'SAP - Módulo MM, gestión de materiales - nivel usuario',
+        hours: 40,
+        entidad_subvencion: "/imgs/logos/logo-socs.svg",
+        init_date: '31-03-2025',
+        end_date: '10-04-2025',
+        requirements: socAda,
+        estado: true
+    },
+    {
+        id: '11',
+        img: '/imgs/cursos/sectorial/sectorial-cliente-ingles.webp',
+        codigo: "[COMT0013]",
+        name: 'Atención al cliente del pequeño comercio en lengua extranjera, Inglés',
+        hours: 30,
+        entidad_subvencion: "/imgs/logos/consorci.png",
+        init_date: "20-03-2025",
+        end_date: "31-03-2025",
+        requirements: consorciSectorial,
+        estado: true
+    },
+    {
+        id: '12',
+        img: '/imgs/cursos/sectorial/sectorial-neuromarketing.webp',
+        codigo: "[COMM0014]",
+        name: 'Neuromarketing curso virtual para empleados',
+        hours: 35,
+        entidad_subvencion: "/imgs/logos/consorci.png",
+        init_date: "31-03-2025",
+        end_date: "14-04-2025",
+        requirements: consorciSectorial,
+        estado: true
+    },
+    {
+        id: '13',
         img: '/imgs/cursos/sectorial/sectorial-ventas-comerciales.webp',
-        codigo: "COMT0017",
+        codigo: "[COMT0017]",
         name: 'Atención y venta en establecimientos comerciales',
         hours: 30,
         entidad_subvencion: "/imgs/logos/consorci.png",
@@ -103,9 +187,9 @@ export const courses: course[] = [
         estado: true
     },
     {
-        id: '7',
+        id: '14',
         img: '/imgs/cursos/sectorial/sectorial-comercio-exterior.webp',
-        codigo: "COMT0015",
+        codigo: "[COMT0015]",
         name: 'Gestión del comercio exterior curso virtual',
         hours: 40,
         entidad_subvencion: "/imgs/logos/consorci.png",
@@ -115,9 +199,9 @@ export const courses: course[] = [
         estado: true
     },
     {
-        id: '8',
+        id: '15',
         img: '/imgs/cursos/sectorial/sectorial-contabilidad-basica.webp',
-        codigo: "ADGD0061",
+        codigo: "[ADGD0061]",
         name: 'Contabilidad básica curso virtual para empleados',
         hours: 60,
         entidad_subvencion: "/imgs/logos/consorci.png",
@@ -127,9 +211,9 @@ export const courses: course[] = [
         estado: true
     },
     {
-        id: '9',
+        id: '16',
         img: '/imgs/cursos/sectorial/sectorial-contabilidad-avanzada.webp',
-        codigo: "ADGD0064",
+        codigo: "[ADGD0064]",
         name: 'Contabilidad avanzada curso virtual para empleados',
         hours: 40,
         entidad_subvencion: "/imgs/logos/consorci.png",
@@ -139,9 +223,9 @@ export const courses: course[] = [
         estado: true
     },
     {
-        id: '10',
+        id: '17',
         img: '/imgs/cursos/sectorial/sectorial-gestion-tesoreria.webp',
-        codigo: "ADGD0026",
+        codigo: "[ADGD0026]",
         name: 'Gestión de tesoreria curso virtual para empleados',
         hours: 60,
         entidad_subvencion: "/imgs/logos/consorci.png",
@@ -151,9 +235,9 @@ export const courses: course[] = [
         estado: true
     },
     {
-        id: '11',
+        id: '18',
         img: '/imgs/cursos/foap/foap-direcció-cordinació-infantil-lleure.webp',
-        codigo: "SSCB0211",
+        codigo: "[SSCB0211]",
         name: 'Dirección/coordinación de actividades de educación en el ocio infantil',
         hours: 290,
         entidad_subvencion: "/imgs/logos/logo-socs.svg",
@@ -163,9 +247,9 @@ export const courses: course[] = [
         estado: true
     },
     {
-        id: '12',
+        id: '19',
         img: '/imgs/cursos/foap/foap-atenció-sociosanitaria-domicili.webp',
-        codigo: "SSCS0108",
+        codigo: "[SSCS0108]",
         name: 'Atención sociosanitaria a personas en el domicilio',
         hours: 480,
         entidad_subvencion: "/imgs/logos/logo-socs.svg",
@@ -175,9 +259,9 @@ export const courses: course[] = [
         estado: true
     },
     {
-        id: '13',
+        id: '20',
         img: '/imgs/cursos/foap/foap-gestión-llamadas-teleasistencia.webp',
-        codigo: "SSCG0111",
+        codigo: "[SSCG0111]",
         name: 'Gestión de llamadas de teleasistencia',
         hours: 230,
         entidad_subvencion: "/imgs/logos/logo-socs.svg",
@@ -187,9 +271,9 @@ export const courses: course[] = [
         estado: true
     },
     {
-        id: '14',
+        id: '21',
         img: '/imgs/cursos/consorci/depilacion_mecanica.webp',
-        codigo: "MF0345",
+        codigo: "[MF0345]",
         name: 'Depilación mecánica y decoloración del vello',
         hours: 90,
         entidad_subvencion: "/imgs/logos/consorci.png",
@@ -199,9 +283,9 @@ export const courses: course[] = [
         estado: true
     },
     {
-        id: '15',
+        id: '22',
         img: '/imgs/cursos/consorci/masaje-estético-manual-mecánico.webp',
-        codigo: "MF0063",
+        codigo: "[MF0063]",
         name: 'Masaje estético manual y mecánico para empleados',
         hours: 90,
         entidad_subvencion: "/imgs/logos/consorci.png",
@@ -211,9 +295,9 @@ export const courses: course[] = [
         estado: true
     },
     {
-        id: '16',
+        id: '23',
         img: '/imgs/cursos/consorci/diagnostico-protocolos-masajes-esteticos-tecnicas-sensoriales.webp',
-        codigo: "MF0789",
+        codigo: "[MF0789]",
         name: 'Diagnóstico y protocolos para masajes estéticos y técnicas sensoriales',
         hours: 90,
         entidad_subvencion: "/imgs/logos/consorci.png",
@@ -223,9 +307,9 @@ export const courses: course[] = [
         estado: true
     },
     {
-        id: '17',
+        id: '24',
         img: '/imgs/cursos/consorci/aplicación-cosméticos-para-cambios-color-cabello.webp',
-        codigo: "MF0060",
+        codigo: "[MF0060]",
         name: 'Aplicación de cosméticos para los cambios de color del cabello',
         hours: 80,
         entidad_subvencion: "/imgs/logos/consorci.png",
@@ -235,10 +319,10 @@ export const courses: course[] = [
         estado: true
     },
     {
-        id: '18',
+        id: '25',
         img: '/imgs/cursos/consorci/análisis-cuero-cabelludo-cabello-protocolos-trabajos-técnicos-curas-capilares.webp',
-        codigo: "MF0347",
-        name: 'Análisis del cuero cabelludo y cabello protocolos de trabajos técnicos',
+        codigo: "[MF0347]",
+        name: 'Análisis del cuero cabelludo y cabello protocolos de trabajos técnicos para empleados',
         hours: 90,
         entidad_subvencion: "/imgs/logos/consorci.png",
         init_date: "09-04-2025",
@@ -247,10 +331,10 @@ export const courses: course[] = [
         estado: true
     },
     {
-        id: '19',
+        id: '26',
         img: '/imgs/cursos/consorci/emisión-gestión-llamadas-salientes-en-un-servicio-teleasistencia.webp',
-        codigo: "MF1424",
-        name: 'Emisión y gestión de llamadas salientes en un servicio de teleasistencia',
+        codigo: "[MF1424]",
+        name: 'Emisión y gestión de llamadas salientes en un servicio de teleasistencia para empleados',
         hours: 60,
         entidad_subvencion: "/imgs/logos/consorci.png",
         init_date: "28-03-2025",
@@ -259,9 +343,9 @@ export const courses: course[] = [
         estado: true
     },
     {
-        id: '20',
+        id: '27',
         img: '/imgs/cursos/consorci/manejo-herramientas-técnicas-habilidades-para-prestar-servicio-teleasistencia.webp',
-        codigo: "MF1425",
+        codigo: "[MF1425]",
         name: 'Manejo de las herramientas, técnicas y habilidades para prestar el servicio de teleasistencia',
         hours: 80,
         entidad_subvencion: "/imgs/logos/consorci.png",
@@ -270,7 +354,7 @@ export const courses: course[] = [
         requirements: consorciEmpleados,
         estado: true
     },
-
+ 
 
 
     

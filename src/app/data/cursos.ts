fa2fde168d72f5676e8b405440e0e10e8a305ae3
victorstudy <victorstudyformacion@gmail.com>
2025-03-05 16:36:55 +0100

    new format of courses on courses database

--- src/app/data/cursos.ts
+++ src/app/data/cursos.ts
@@ -152,8 +152,44 @@ export const courses: course[] = [
     },
     {
         id: '11',
+        img: '/imgs/cursos/sectorial/sectorial-comercio-exterior.webp',
+        codigo: "[COMT0015_1]",
+        name: 'Gestión del comercio exterior curso virtual',
+        hours: 40,
+        entidad_subvencion: "/imgs/logos/consorci.png",
+        init_date: "14-03-2025",
+        end_date: "02-04-2025",
+        requirements: consorciSectorial,
+        estado: true
+    },
+    {
+        id: '12',
+        img: '/imgs/cursos/sectorial/sectorial-gestion-empresa.webp',
+        codigo: "[ADGD0040_1]",
+        name: 'Gestión fiscal de empresa curso virtual',
+        hours: 40,
+        entidad_subvencion: "/imgs/logos/consorci.png",
+        init_date: "04-04-2025",
+        end_date: "22-04-2025",
+        requirements: consorciSectorial,
+        estado: true
+    },
+    {
+        id: '13',
+        img: '/imgs/cursos/sectorial/sectorial-fiscalidad-operativa.webp',
+        codigo: "[ADGN0010_1]",
+        name: 'Fiscalidad operativa curso virtual',
+        hours: 60,
+        entidad_subvencion: "/imgs/logos/consorci.png",
+        init_date: "24-04-2025",
+        end_date: "14-05-2025",
+        requirements: consorciSectorial,
+        estado: true
+    },
+    {
+        id: '14',
         img: '/imgs/cursos/sectorial/sectorial-cliente-ingles.webp',
-        codigo: "[COMT0013]",
+        codigo: "[COMT0013_3]",
         name: 'Atención al cliente del pequeño comercio en lengua extranjera, Inglés',
         hours: 30,
         entidad_subvencion: "/imgs/logos/consorci.png",
@@ -163,9 +199,33 @@ export const courses: course[] = [
         estado: true
     },
     {
-        id: '12',
+        id: '15',
+        img: '/imgs/cursos/sectorial/sectorial-atencion-venta-establecimientos.webp',
+        codigo: "[COMT0017_3]",
+        name: 'Atención y venta a establecimientos comerciales',
+        hours: 30,
+        entidad_subvencion: "/imgs/logos/consorci.png",
+        init_date: "01-04-2025",
+        end_date: "11-04-2025",
+        requirements: consorciSectorial,
+        estado: true
+    },
+    {
+        id: '16',
+        img: '/imgs/cursos/sectorial/sectorial-tecnicas-venta-habiidades-comerciales.webp',
+        codigo: "[COMT0016_3]",
+        name: 'Técnicas para la venta y habilidades comerciales',
+        hours: 40,
+        entidad_subvencion: "/imgs/logos/consorci.png",
+        init_date: "14-04-2025",
+        end_date: "28-04-2025",
+        requirements: consorciSectorial,
+        estado: true
+    },
+    {
+        id: '17',
         img: '/imgs/cursos/sectorial/sectorial-neuromarketing.webp',
-        codigo: "[COMM0014]",
+        codigo: "[COMM0014_4]",
         name: 'Neuromarketing curso virtual para empleados',
         hours: 35,
         entidad_subvencion: "/imgs/logos/consorci.png",
@@ -175,79 +235,43 @@ export const courses: course[] = [
         estado: true
     },
     {
-        id: '13',
-        img: '/imgs/cursos/sectorial/sectorial-ventas-comerciales.webp',
-        codigo: "[COMT0017]",
-        name: 'Atención y venta en establecimientos comerciales',
-        hours: 30,
-        entidad_subvencion: "/imgs/logos/consorci.png",
-        init_date: "Por definir",
-        end_date: "Por definir",
-        requirements: consorciSectorial,
-        estado: true
-    },
-    {
-        id: '14',
-        img: '/imgs/cursos/sectorial/sectorial-comercio-exterior.webp',
-        codigo: "[COMT0015]",
-        name: 'Gestión del comercio exterior curso virtual',
-        hours: 40,
-        entidad_subvencion: "/imgs/logos/consorci.png",
-        init_date: "Por definir",
-        end_date: "Por definir",
-        requirements: consorciSectorial,
-        estado: true
-    },
-    {
-        id: '15',
-        img: '/imgs/cursos/sectorial/sectorial-contabilidad-basica.webp',
-        codigo: "[ADGD0061]",
-        name: 'Contabilidad básica curso virtual para empleados',
-        hours: 60,
-        entidad_subvencion: "/imgs/logos/consorci.png",
-        init_date: "16-12-2024",
-        end_date: "04-01-2025",
-        requirements: consorciSectorial,
-        estado: true
-    },
-    {
-        id: '16',
-        img: '/imgs/cursos/sectorial/sectorial-contabilidad-avanzada.webp',
-        codigo: "[ADGD0064]",
-        name: 'Contabilidad avanzada curso virtual para empleados',
-        hours: 40,
-        entidad_subvencion: "/imgs/logos/consorci.png",
-        init_date: "05-01-2025",
-        end_date: "23-01-2025",
-        requirements: consorciSectorial,
-        estado: true
-    },
-    {
-        id: '17',
-        img: '/imgs/cursos/sectorial/sectorial-gestion-tesoreria.webp',
-        codigo: "[ADGD0026]",
-        name: 'Gestión de tesoreria curso virtual para empleados',
-        hours: 60,
-        entidad_subvencion: "/imgs/logos/consorci.png",
-        init_date: "Por definir",
-        end_date: "Por definir",
-        requirements: consorciSectorial,
-        estado: true
-    },
-    {
         id: '18',
+        img: '/imgs/cursos/sectorial/sectorial-marketing-buscadores-seo-sem.webp',
+        codigo: "[COMM41_4]",
+        name: 'Marketing Y Buscadores: SEO, SEM Y Analítica Web',
+        hours: 45,
+        entidad_subvencion: "/imgs/logos/consorci.png",
+        init_date: "16-04-2025",
+        end_date: "05-05-2025",
+        requirements: consorciSectorial,
+        estado: true
+    },
+    {
+        id: '19',
+        img: '/imgs/cursos/sectorial/sectorial-tienda-virtual-gestion-comercio.webp',
+        codigo: "[COMM41_4]",
+        name: 'Tienda virtual y gestión del comercio electrónico',
+        hours: 35,
+        entidad_subvencion: "/imgs/logos/consorci.png",
+        init_date: "07-05-2025",
+        end_date: "20-05-2025",
+        requirements: consorciSectorial,
+        estado: true
+    },
+    {
+        id: '20',
         img: '/imgs/cursos/foap/foap-direcció-cordinació-infantil-lleure.webp',
         codigo: "[SSCB0211]",
         name: 'Dirección/coordinación de actividades de educación en el ocio infantil',
         hours: 290,
         entidad_subvencion: "/imgs/logos/logo-socs.svg",
-        init_date: "Por definir",
-        end_date: "Por definir",
+        init_date: "13-02-2025",
+        end_date: "30-06-2025",
         requirements: socAda,
         estado: true
     },
     {
-        id: '19',
+        id: '21',
         img: '/imgs/cursos/foap/foap-atenció-sociosanitaria-domicili.webp',
         codigo: "[SSCS0108]",
         name: 'Atención sociosanitaria a personas en el domicilio',
@@ -259,19 +283,19 @@ export const courses: course[] = [
         estado: true
     },
     {
-        id: '20',
+        id: '22',
         img: '/imgs/cursos/foap/foap-gestión-llamadas-teleasistencia.webp',
         codigo: "[SSCG0111]",
         name: 'Gestión de llamadas de teleasistencia',
         hours: 230,
         entidad_subvencion: "/imgs/logos/logo-socs.svg",
         init_date: "27-06-2025",
-        end_date: "Por definir",
+        end_date: "07-11-2025",
         requirements: socAda,
         estado: true
     },
     {
-        id: '21',
+        id: '23',
         img: '/imgs/cursos/consorci/depilacion_mecanica.webp',
         codigo: "[MF0345]",
         name: 'Depilación mecánica y decoloración del vello',
@@ -283,7 +307,7 @@ export const courses: course[] = [
         estado: true
     },
     {
-        id: '22',
+        id: '24',
         img: '/imgs/cursos/consorci/masaje-estético-manual-mecánico.webp',
         codigo: "[MF0063]",
         name: 'Masaje estético manual y mecánico para empleados',
@@ -295,7 +319,7 @@ export const courses: course[] = [
         estado: true
     },
     {
-        id: '23',
+        id: '25',
         img: '/imgs/cursos/consorci/diagnostico-protocolos-masajes-esteticos-tecnicas-sensoriales.webp',
         codigo: "[MF0789]",
         name: 'Diagnóstico y protocolos para masajes estéticos y técnicas sensoriales',
@@ -307,7 +331,7 @@ export const courses: course[] = [
         estado: true
     },
     {
-        id: '24',
+        id: '26',
         img: '/imgs/cursos/consorci/aplicación-cosméticos-para-cambios-color-cabello.webp',
         codigo: "[MF0060]",
         name: 'Aplicación de cosméticos para los cambios de color del cabello',
@@ -319,7 +343,7 @@ export const courses: course[] = [
         estado: true
     },
     {
-        id: '25',
+        id: '27',
         img: '/imgs/cursos/consorci/análisis-cuero-cabelludo-cabello-protocolos-trabajos-técnicos-curas-capilares.webp',
         codigo: "[MF0347]",
         name: 'Análisis del cuero cabelludo y cabello protocolos de trabajos técnicos para empleados',
@@ -331,7 +355,7 @@ export const courses: course[] = [
         estado: true
     },
     {
-        id: '26',
+        id: '28',
         img: '/imgs/cursos/consorci/emisión-gestión-llamadas-salientes-en-un-servicio-teleasistencia.webp',
         codigo: "[MF1424]",
         name: 'Emisión y gestión de llamadas salientes en un servicio de teleasistencia para empleados',
@@ -343,7 +367,7 @@ export const courses: course[] = [
         estado: true
     },
     {
-        id: '27',
+        id: '29',
         img: '/imgs/cursos/consorci/manejo-herramientas-técnicas-habilidades-para-prestar-servicio-teleasistencia.webp',
         codigo: "[MF1425]",
         name: 'Manejo de las herramientas, técnicas y habilidades para prestar el servicio de teleasistencia',
